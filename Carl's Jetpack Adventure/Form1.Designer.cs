427bd098da805455f67c5cec506373ff00ed35c1
Murilo Trevejo Santos <55139539+Trevejo@users.noreply.github.com>
2022-11-20 13:28:52 -0300

    Add files via upload

---
+++ Carl's Jetpack Adventure/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace Carl_s_Jetpack_Adventure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.coinText = new System.Windows.Forms.Label();
+            this.carlCollider = new System.Windows.Forms.PictureBox();
+            this.zapper2 = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.zapper1 = new System.Windows.Forms.PictureBox();
+            this.carl = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.carlCollider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zapper2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zapper1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // coinText
+            // 
+            this.coinText.AutoSize = true;
+            this.coinText.Location = new System.Drawing.Point(12, 9);
+            this.coinText.Name = "coinText";
+            this.coinText.Size = new System.Drawing.Size(35, 13);
+            this.coinText.TabIndex = 5;
+            this.coinText.Text = "label1";
+            // 
+            // carlCollider
+            // 
+            this.carlCollider.Location = new System.Drawing.Point(128, 155);
+            this.carlCollider.Name = "carlCollider";
+            this.carlCollider.Size = new System.Drawing.Size(35, 55);
+            this.carlCollider.TabIndex = 6;
+            this.carlCollider.TabStop = false;
+            // 
+            // zapper2
+            // 
+            this.zapper2.Location = new System.Drawing.Point(706, 64);
+            this.zapper2.Name = "zapper2";
+            this.zapper2.Size = new System.Drawing.Size(49, 113);
+            this.zapper2.TabIndex = 4;
+            this.zapper2.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Location = new System.Drawing.Point(-6, 404);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(813, 50);
+            this.ground.TabIndex = 2;
+            this.ground.TabStop = false;
+            // 
+            // zapper1
+            // 
+            this.zapper1.Location = new System.Drawing.Point(411, 255);
+            this.zapper1.Name = "zapper1";
+            this.zapper1.Size = new System.Drawing.Size(48, 120);
+            this.zapper1.TabIndex = 1;
+            this.zapper1.TabStop = false;
+            // 
+            // carl
+            // 
+            this.carl.Location = new System.Drawing.Point(78, 139);
+            this.carl.Name = "carl";
+            this.carl.Size = new System.Drawing.Size(109, 90);
+            this.carl.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.carl.TabIndex = 0;
+            this.carl.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.carlCollider);
+            this.Controls.Add(this.coinText);
+            this.Controls.Add(this.zapper2);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.zapper1);
+            this.Controls.Add(this.carl);
+            this.Name = "Form1";
+            this.Text = "Carl\'s Jetpack Adventure";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.carlCollider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zapper2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zapper1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carl)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox carl;
+        private System.Windows.Forms.PictureBox zapper1;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox zapper2;
+        private System.Windows.Forms.Label coinText;
+        private System.Windows.Forms.PictureBox carlCollider;
+    }
+}
+
---
+++ Carl's Jetpack Adventure/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Carl_s_Jetpack_Adventure
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        int gameSpeed = 8; 
+        int gravity = 7;
+        int coins = 0;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Space)
+            {
+
+                gravity = -7;
+                for (int i = 0; i <= 14; i++)
+                {
+                    carl.Image = Image.FromFile($"C:\\Users\\muril\\Desktop\\Assets jogo\\flying-frames\\tile0{i}.png");
+                }
+            }
+        }
+
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 7;
+                carl.Image = Image.FromFile("C:\\Users\\muril\\Desktop\\Assets jogo\\idle-frame.png");
+            }
+        }
+
+        private void endGame()
+        {
+            gameTimer.Stop();
+            coinText.Text += " Game over!!!"; 
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            carl.Top += gravity;
+            carlCollider.Top += gravity;
+            zapper1.Left -= gameSpeed;
+            zapper2.Left -= gameSpeed; 
+            coinText.Text = "Coins: " + coins;
+
+            if (zapper1.Left < -150)
+            {
+                
+                zapper1.Left = 800;
+                coins++;
+            }
+            if (zapper2.Left < -180)
+            {
+                zapper2.Left = 950;
+                coins++;
+            }
+
+            if (carlCollider.Bounds.IntersectsWith(zapper1.Bounds) ||
+                carlCollider.Bounds.IntersectsWith(zapper2.Bounds) ||
+                carlCollider.Bounds.IntersectsWith(ground.Bounds) || carlCollider.Top < -25
+                )
+            {
+                endGame();
+            }
+
+            if (coins > 5)
+            {
+                gameSpeed = 15;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            carl.BringToFront();
+            zapper1.Image = Image.FromFile($"C:\\Users\\muril\\Desktop\\Assets jogo\\zapper-frames\\Zapper2.png");
+            zapper2.Image = Image.FromFile($"C:\\Users\\muril\\Desktop\\Assets jogo\\zapper-frames\\Zapper2.png");
+        }
+    }
+}
