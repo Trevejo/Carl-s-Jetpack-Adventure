4fdbd2321c681ca428d9a7b838225c4734afce21
Murilo Trevejo Santos <55139539+Trevejo@users.noreply.github.com>
2022-11-21 13:10:41 -0300

    Add files via upload

---
+++ Carls_Jetpack_Adventure_v0.2/Carl's Jetpack Adventure/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Carl_s_Jetpack_Adventure
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new GameMenu());
+        }
+    }
+}
